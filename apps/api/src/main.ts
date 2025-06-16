cf24c3239329e1b2f79097ef534e57e3e8252791
Alessandro Prudencio <alessandroconectado@gmail.com>
2025-06-16 15:31:57 -0400

    docs(api): update endpoint for docs

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup('docs', app, document);
 
   // Configuração de validação global
   app.useGlobalPipes(
