4204a365a77e6519bc7f549d8bb28d5f574934d5
Alessandro Prudencio <alessandroconectado@gmail.com>
2025-06-23 09:17:38 -0400

    feat(api/prisma): enhance seed script to check for existing admin user and categories before creation, and prevent duplicate media entries during seeding

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -1,29 +1,35 @@
-import { PrismaClient, MediaType, MediaRating } from '@prisma/client';
-import * as bcrypt from 'bcrypt';
+import { PrismaClient, MediaType, MediaRating, Category } from '@prisma/client'
+import * as bcrypt from 'bcrypt'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 async function main() {
-  // Criar usuário admin
-  const adminUser = await prisma.user.create({
-    data: {
-      name: 'Admin',
-      email: 'admin@example.com',
-      password: await bcrypt.hash('93+~N5!a', 10),
-    },
-  });
+  // Criar usuário admin se não existir
+  let adminUser = await prisma.user.findUnique({
+    where: { email: 'admin@example.com' },
+  })
+  if (!adminUser) {
+    adminUser = await prisma.user.create({
+      data: {
+        name: 'Admin',
+        email: 'admin@example.com',
+        password: await bcrypt.hash('93+~N5!a', 10),
+      },
+    })
+  }
 
-  // Create categories
-  const categories = await Promise.all([
-    prisma.category.create({ data: { name: 'Ação' } }),
-    prisma.category.create({ data: { name: 'Aventura' } }),
-    prisma.category.create({ data: { name: 'Comédia' } }),
-    prisma.category.create({ data: { name: 'Drama' } }),
-    prisma.category.create({ data: { name: 'Ficção Científica' } }),
-    prisma.category.create({ data: { name: 'Suspense' } }),
-  ]);
+  // Create categories se não existirem
+  const categoryNames = ['Ação', 'Aventura', 'Comédia', 'Drama', 'Ficção Científica', 'Suspense']
+  const categories: Category[] = []
+  for (const name of categoryNames) {
+    let category: Category | null = await prisma.category.findUnique({ where: { name } })
+    if (!category) {
+      category = await prisma.category.create({ data: { name } })
+    }
+    categories.push(category)
+  }
 
-  // Criar mídias
+  // Criar mídias se não existirem
   const media = [
     {
       title: 'O Poderoso Chefão',
@@ -31,7 +37,8 @@ async function main() {
         'A história da família Corleone, uma das mais poderosas famílias do crime organizado nos Estados Unidos.',
       thumbnailUrl:
         'https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1925&q=80',
-      poster: 'https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1925&q=80',
+      poster:
+        'https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1925&q=80',
       releaseYear: 1972,
       type: MediaType.MOVIE,
       rating: MediaRating.R,
@@ -48,7 +55,8 @@ async function main() {
         'Um professor de química do ensino médio é diagnosticado com câncer e começa a fabricar metanfetamina para garantir o futuro financeiro de sua família.',
       thumbnailUrl:
         'https://images.unsplash.com/photo-1626814026160-2237a95fc5a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
-      poster: 'https://images.unsplash.com/photo-1626814026160-2237a95fc5a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+      poster:
+        'https://images.unsplash.com/photo-1626814026160-2237a95fc5a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
       releaseYear: 2008,
       type: MediaType.SERIES,
       rating: MediaRating.R,
@@ -62,8 +70,7 @@ async function main() {
       title: 'Interestelar',
       description:
         'Um grupo de astronautas viaja através de um buraco de minhoca no espaço para garantir a sobrevivência da humanidade.',
-      thumbnailUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/3/38/Stranger_Things_logo.png',
+      thumbnailUrl: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Stranger_Things_logo.png',
       poster: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Stranger_Things_logo.png',
       releaseYear: 2014,
       type: MediaType.MOVIE,
@@ -78,8 +85,7 @@ async function main() {
       title: 'Duna: Parte 2',
       description:
         'Paul Atreides une forças com Chani e os Fremen para liderar a revolta contra aqueles que destruíram sua família.',
-      thumbnailUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/3/38/Stranger_Things_logo.png',
+      thumbnailUrl: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Stranger_Things_logo.png',
       poster: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Stranger_Things_logo.png',
       releaseYear: 2024,
       type: MediaType.MOVIE,
@@ -94,8 +100,7 @@ async function main() {
       title: 'Stranger Things',
       description:
         'Quando um garoto desaparece, sua mãe, um chefe de polícia e seus amigos devem enfrentar forças sobrenaturais terríveis para recuperá-lo.',
-      thumbnailUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/3/38/Stranger_Things_logo.png',
+      thumbnailUrl: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Stranger_Things_logo.png',
       poster: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Stranger_Things_logo.png',
       releaseYear: 2016,
       type: MediaType.SERIES,
@@ -106,32 +111,37 @@ async function main() {
       userId: adminUser.id,
       categoryIds: [categories[4].id, categories[5].id],
     },
-  ];
+  ]
 
   for (const item of media) {
-    const { categoryIds, userId, ...mediaData } = item;
-    await prisma.media.create({
-      data: {
-        ...mediaData,
-        user: {
-          connect: { id: userId }
+    const { categoryIds, userId, title, ...mediaData } = item
+    // Verifica se já existe mídia com o mesmo título
+    let existingMedia = await prisma.media.findFirst({ where: { title } })
+    if (!existingMedia) {
+      await prisma.media.create({
+        data: {
+          ...mediaData,
+          title,
+          user: {
+            connect: { id: userId },
+          },
+          categories: {
+            create: categoryIds.map(categoryId => ({
+              categoryId,
+              mediaId: undefined, // This will be set automatically by Prisma
+            })),
+          },
         },
-        categories: {
-          create: categoryIds.map((categoryId) => ({
-            categoryId,
-            mediaId: undefined // This will be set automatically by Prisma
-          }))
-        }
-      }
-    });
+      })
+    }
   }
 }
 
 main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
   })
   .finally(async () => {
-    await prisma.$disconnect();
-  });
+    await prisma.$disconnect()
+  })
