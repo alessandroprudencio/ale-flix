80f462f41bd57c92298a9adf48671b35e1754f28
Alessandro Prudencio <alessandroconectado@gmail.com>
2025-06-24 07:54:52 -0400

    feat(docker): add nginx CORS proxy and Redis service to Docker setup, configure OpenTelemetry environment variables, and update media seed script with stream URLs

--- apps/web/plugins/otel.client.ts
+++ apps/web/plugins/otel.client.ts
@@ -1,4 +1,3 @@
-// plugins/otel.client.ts
 import { WebTracerProvider } from '@opentelemetry/sdk-trace-web'
 import { SimpleSpanProcessor, ConsoleSpanExporter } from '@opentelemetry/sdk-trace-base'
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
@@ -6,12 +5,20 @@ import { registerInstrumentations } from '@opentelemetry/instrumentation'
 import { FetchInstrumentation } from '@opentelemetry/instrumentation-fetch'
 import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load'
 import { XMLHttpRequestInstrumentation } from '@opentelemetry/instrumentation-xml-http-request'
+import { resourceFromAttributes } from '@opentelemetry/resources'
+import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions'
+import { UserInteractionInstrumentation } from '@opentelemetry/instrumentation-user-interaction'
 
 export default defineNuxtPlugin(() => {
   const otlpExporter = new OTLPTraceExporter({
-    url: 'http://ale-flix-jaeger:4318/v1/traces',
+    url: 'http://localhost:8082/v1/traces',
+  })
+
+  const resource = resourceFromAttributes({
+    [ATTR_SERVICE_NAME]: 'ale-flix-web',
   })
   const provider = new WebTracerProvider({
+    resource,
     spanProcessors: [new SimpleSpanProcessor(otlpExporter)],
   })
 
@@ -20,11 +27,12 @@ export default defineNuxtPlugin(() => {
 
   registerInstrumentations({
     instrumentations: [
+      new UserInteractionInstrumentation(),
       new DocumentLoadInstrumentation(),
       new FetchInstrumentation(),
       new XMLHttpRequestInstrumentation(),
     ],
   })
 
-  console.log('[OTel] Tracing inicializado no client Nuxt')
+  console.log('[OTel] Tracing inicializado no client Nuxt - Serviço: AleFlix Frontend')
 })
